cab5abea495992bda10ddca10741303a6f82a07f
Gibolt <tjrwh2@gmail.com>
2012-10-24 20:28:58 -0500

    Folder crawling and file reading fixed

--- 397Crawlv1_0.pl
+++ 397Crawlv1_0.pl
@@ -6,29 +6,27 @@
 # -s is also considered 'retro', many programmers prefer to load 
 # a separate module (from the Getopt:: family) for switch parsing. 
 
+# Following line can be used to keep a log
 #open FILES, ">", "files.txt" or die $!; open DIRECTORIES, ">", "directories.txt" or die $!; 
-use Cwd; # module for finding the current working directory 
+use Cwd;
 @videoTypes = (".avi",".mp4",".mkv",".mov",".wmv",".flv");
 @audioTypes = (".wav",".mp3",".flac",".midi",".aac",".m4a",".mp4");
 @imageTypes = (".gif",".jpg",".jpeg",".png",".ico",".bmp",".m4a",".mp4");
-#@acceptedTypes = (".gif",".jpg",".png",".ico",".css",".sit",".eps",".wmf",".mpg",".gz",".rpm",".tgz",".mov", ".exe",".jpeg",".bmp",".js"); 
 @acceptedTypes = (@videoTypes,@audioTypes,@imageTypes);
 %acceptedTypesMap = map { $_ => 1 } @acceptedTypes;
-#@excludedFileNames = ("crawlFilesystem.p1", "directories.txt", "files.txt"); 
-#@excludedTypes = gif","GIF","jpg","JPG","png","PNG","ico","ICO","css","CSS","sit","SIT","eps","EPS","wmf", "WMF","zip","ZIP","ppt","PPT","mpg","MPG","xls","XLS","gz","GZ","rpm","RPM","tgz","TGZ","mo v","MOV","exe","EXE","jpeg","JPEG","bmp","BMP","js","JS"); 
 
 if (1>=$#ARGV+1) {
+	chdir($ARGV[0]);
 	&ScanDirectory($ARGV[0]);
 }
 
-
-# This subroutine takes the name of a directory and recursively scans 
-# down the filesystem 
-sub ScanDirectory{
+# This function takes the name of a directory and recursively scans down the filesystem hierarchy
+sub ScanDirectory {
 	my ($workdir) = shift; 
-	my ($startdir) = &cwd; # keep track of where we began 
-	
 	chdir($workdir) or die "Unable to enter dir $workdir:$!\n";
+	my ($startdir) = &cwd; # keep track of where we began 
+	print "Entered directory: $workdir\n";
+
 	opendir(DIR, ".") or die "Unable to open $workdir:$!\n";
 	my @names = readdir(DIR) or die "Unable to read $workdir:$!\n";
 	closedir(DIR); 
@@ -37,27 +35,29 @@ sub ScanDirectory{
 		next if ($name eq "."); 
 		next if ($name eq ".."); 
 
-		if (-d $name){ 		# is this a directory? 
-			print  "Found directory: $name in $workdir!\n"; 
+		if (-d $name){ 		# Is the given name a directory?
+			print  "Found directory:   $name in $workdir!\n"; 
 			&ScanDirectory($name); 
 			next; 
 		}
-		elsif (-f $name) {
-			# print "Found file: $name in $workdir!\n";
+		elsif (-f $name) {	# Is the given name a file?
 			my $ext;
 			if ($name =~ /(\.[^.]+)$/) {
 				$ext = $1;
 			}
-			print "Found file: $name in $workdir! - $ext\n";
 			my $fullPath = $startdir.'/'.$workdir.'/'.$name;
 			if(exists($acceptedTypesMap{$ext})) {
-
-				print "Found file: $name in $workdir!\n";
+				print "Found acceptable media file: $name in $workdir!\n";
 				# TODO: Send files to appropriate parsing scripts and online syncs
 			}
+			else {
+				print "Found unmatched file:        $name in $workdir!\n";
+			}
 		}
-		print $name."\n";
-		chdir($startdir) or die "Unable to change to dir $startdir:$!\n"; 
+		else {				# Is the given name something else?
+			print "Found some unknown file:      $name\n";
+		}
 	}
+	chdir($startdir) or die "Unable to change to dir $startdir:$!\n"; 
 }
-#&ScanDirectory("."); 
+ 
