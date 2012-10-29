80fc49074577ede66d23e37ce21b622fc7452be5
Gibolt <tjrwh2@gmail.com>
2012-10-29 00:36:01 -0500

    Reduced to main functionality

--- musicApiSearch.pl
+++ musicApiSearch.pl
@@ -1,20 +1,12 @@
 #!/usr/bin/perl -w
 # $Author: Thomas Reese $
-# $Date: 2012-10-27 $
+# $Date: 2012-10-29 $
 
-use XML::SIMPLE;
 use strict;
+use XML::SIMPLE;
 use LWP::UserAgent;
 use Data::Dumper;
 
-
-my $file = 'files/camelids.xml';
-
-
-# foreach my $key (keys (%{$doc->{species}})){
-   # print $doc->{species}->{$key}->{'common-name'} . ' (' . $key . ') ';
-   # print $doc->{species}->{$key}->{conservation}->final . "\n";
-# }
 my $ua = new LWP::UserAgent;
 $ua->timeout(120); 
 my $xs1 = XML::Simple->new();
@@ -26,13 +18,13 @@ for (my $i = 0; $i<$#ARGV+1; $i++) {
 
 sub fetch_title {
 	my $titleIn = $_[0];
-	print $titleIn;
-	$titleIn =~ s/%20/ /g;
-	$titleIn =~ s/-/ /g;
+	$titleIn =~ s/%20|-/ /g;
+	# $titleIn =~ s/-/ /g;
 	$titleIn =~ s/\s+/ /g;
-	print $titleIn;
-	my $mainUrl = "http://musicbrainz.org/ws/2/work/?query=";
-	fetch_xml_page($mainUrl.$titleIn."&limit=1");
+	my $mainUrl = "http://musicbrainz.org/ws/2/work/?limit=1&query=";
+	my $queryUrl = $mainUrl.$titleIn;
+	print "Fetching data for file \"$titleIn\" to url \"$queryUrl\" :\n";
+	fetch_xml_page($queryUrl);
 }
 
 sub fetch_xml_page {
@@ -41,21 +33,10 @@ sub fetch_xml_page {
 	my $response = $ua->request($request);
 	my $musicData = $response->content();
   
-	# if ($musicData =~ m/(<work> .*<work)/) {
-		# my $reduced = $1;
-		# print "\n--".$reduced."--\n";
-	# }
-	print "Getting xml $xml_url\n\n";
 	print $musicData."\n";
 	my $data = $xs1->XMLin($musicData);
-	# my $data = $json->allow_nonref->utf8->relaxed->escape_slash->loose->allow_singlequote->allow_barekey->decode($movieData);
 
-	print Dumper $data."\n";
-    # print $data->{'work'};#->{'title'} . ' (' . $key . ') ';
-	# foreach my $key (keys (%{$data->{'work-list'}})){
-		# print $key."\n";
-	    # print $data->{'species'}->{$key}->{'conservation'}->final . "\n";
-	# }
+	print "\nKeys at each step:\n";
 	print keys $data;
 	print "\n";# print $data->{'Title'}."\n";
 	print keys $data->{'work-list'};# print $data->{'Title'}."\n";
