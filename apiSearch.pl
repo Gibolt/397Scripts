425560f5f1ae59c513175a6a0cdac193a8f5dec9
Gibolt <tjrwh2@gmail.com>
2012-11-18 02:01:46 -0600

    IMDB Search added

--- apiSearch.pl
+++ apiSearch.pl
@@ -4,8 +4,9 @@
 
 use JSON -support_by_pp;
 use strict;
-use LWP::UserAgent;
+use LWP::Simple;
 use Data::Dumper;
+use IMDB::Film; # Needs to be installed with dependencies Cache::CacheUtils, Text::Unidecode, and Error
 
 my $ua = new LWP::UserAgent;
 my $apikey = "rfbnqr2xpkahkypty6m6r3ee";
@@ -18,46 +19,49 @@ for (my $i = 0; $i<$#ARGV+1; $i++) {
 
 sub fetch_title {
 	my $titleIn = $_[0];
-	print $titleIn;
-	$titleIn =~ s/%20/ /g;
-	$titleIn =~ s/-/ /g;
-	$titleIn =~ s/\s+/ /g;
-	print $titleIn;
+	# $titleIn =~ s/%20/ /g;
+	$titleIn =~ s/(%20|\s|-|_)+/ /g;
+	# $titleIn =~ s/\s+/ /g;
+	print $titleIn ;
 	# Check out Thetvdb.com for tv shows
 	my $mainUrl = "http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=".$apikey;
-	fetch_json_page($mainUrl."&q="."$titleIn"."&page_limit=1");
+	# fetch_rt_page($mainUrl."&q="."$titleIn"."&page_limit=1");
+	# fetch_imdb_page($titleIn);
 }
 
-sub fetch_json_page {
+sub fetch_rt_page {
 	my ($json_url) = @_;
 	my $request = new HTTP::Request('GET', $json_url); 
 	my $response = $ua->request($request);
 	my $movieData = $response->content();
   
-	print "Getting json $json_url\n\n";
+	# print "Getting json $json_url\n\n";
 	my $json = new JSON;
 	my $data = $json->allow_nonref->utf8->relaxed->escape_slash->loose->allow_singlequote->allow_barekey->decode($movieData);
 
 	print Dumper $data;
-	# print keys $data;
-	# print $data->{'Title'}."\n";
-	# print $data->{'Year'}."\n";
-	
-	# my $title = $data->{'Title'};
-	# my $year = $data->{'Year'};
-	# my $released = $data->{'Released'};
-	# my $genre = $data->{'Genre'};
-	# my $actors = $data->{'Actors'};
-	# my $director = $data->{'Director'};
-	# my $rating = $data->{'Rated'};
-	# my $writer = $data->{'Writer'};
-	# my $runtime = $data->{'Runtime'};
-	# my $plot = $data->{'Plot'};
-	# my $imdb = $data->{'imdbID'};
-	# my $votes = $data->{'imdbVotes'};
-	# my $poster = $data->{'Poster'};
-	# my $rated = $data->{'Rated'};
-	
-	# print $votes;
+	if (($data -> {'total'}) > 0) {
+		print $data -> {'movies'}[0] -> {'alternate_ids'} -> {'imdb'};
+		print "\n";
+			# Fetch movie posters
+		# getstore($data -> {'movies'}[0] -> {'posters'} -> {'profile'}, "profile.jpg");
+		# getstore($data -> {'movies'}[0] -> {'posters'} -> {'detailed'}, "detailed.jpg");
+		# getstore($data -> {'movies'}[0] -> {'posters'} -> {'thumbnail'}, "thumbnail.jpg");
+		# getstore($data -> {'movies'}[0] -> {'posters'} -> {'original'}, "original.jpg");
+		fetch_imdb_page($data -> {'movies'}[0] -> {'alternate_ids'} -> {'imdb'});  
+	}
+}
+
+sub fetch_imdb_page {
+	my ($movieId) = @_;
+	my $imdbObj = new IMDB::Film(crit => $movieId); 
   
+	print Dumper $imdbObj;
+	if($imdbObj->status) {
+        print "\nTitle: ".$imdbObj->title()."\n";
+        print "Year: ".$imdbObj->year()."\n";
+        print "Plot Summary: ".$imdbObj->plot()."\n";
+	} else {
+		print "Something wrong: ".$imdbObj->error;
+	}
 }
